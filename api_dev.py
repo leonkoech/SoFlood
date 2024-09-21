125f3527e46115f3b51c706846eb03f32f6709ab
Elvis Hernandez <elvishernandezdev@gmail.com>
2024-09-20 22:42:25 -0400

    fetch address lat lng

--- api_dev.py
+++ api_dev.py
@@ -1,11 +1,19 @@
 from flask import Blueprint, request, jsonify
+import requests
+import os
 
 geocode_bp = Blueprint('geocode', __name__)
+GEOCODING_API_KEY = os.environ.get('GOOGLE_GEOCODING_API_KEY')
 
 @geocode_bp.route('/geocode', methods=['GET'])
 def get_geocode_data():
     address = request.args.get('address')
-    print(f'address={address}')
+    response = requests.get(f'https://maps.googleapis.com/maps/api/geocode/json?key={GEOCODING_API_KEY}&address={address}')
+    results = response.json()["results"]
 
-    return jsonify({ "ok": True, "address": address }), 200
+    latlng = None
+    if results:
+        latlng = results[0]["geometry"]["location"]
+
+    return jsonify({ "ok": True, "address": address, "latlng": latlng }), 200
 

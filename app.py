103841722fbb4f8abad4c37d37812daae6d1a226
Leon Kipkoech <leonkipkip@gmail.com>
2024-09-20 21:08:55 -0400

    added files for dev

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify, render_template
 import api  # Importing the file `api.py`
+import api_dev
+import api_dev_2
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+# this is an example of a get request
+#  go to this link to see: http://127.0.0.1:5000/get_test
+@app.route('/get_test',  methods=['GET'])
+def get_test():
+    try:
+        data = {"message": "this is my data"}
+        return jsonify(data), 200
+    except Exception as e:
+        error_message = {"error": str(e)}
+        return jsonify(error_message), 500
+
 
 if __name__ == '__main__':
     app.run(debug=True)

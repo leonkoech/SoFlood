e4c1606b840f0821764313707643832af4eab2ab
Elvis Hernandez <elvishernandezdev@gmail.com>
2024-09-20 22:45:41 -0400

    Improve endpoint structure

--- api_dev.py
+++ api_dev.py
@@ -7,13 +7,26 @@ GEOCODING_API_KEY = os.environ.get('GOOGLE_GEOCODING_API_KEY')
 
 @geocode_bp.route('/geocode', methods=['GET'])
 def get_geocode_data():
-    address = request.args.get('address')
-    response = requests.get(f'https://maps.googleapis.com/maps/api/geocode/json?key={GEOCODING_API_KEY}&address={address}')
-    results = response.json()["results"]
 
-    latlng = None
-    if results:
-        latlng = results[0]["geometry"]["location"]
+    response = {
+        "error": None, 
+        "data": None
+    }
+    status_code = 200
 
-    return jsonify({ "ok": True, "address": address, "latlng": latlng }), 200
+    try:
+        address = request.args.get('address')
+        geocode_data = requests.get(f'https://maps.googleapis.com/maps/api/geocode/json?key={GEOCODING_API_KEY}&address={address}')
+        results = geocode_data.json()["results"]
+        latlng = None
+        if results:
+            latlng = results[0]["geometry"]["location"]
+            response["data"] = latlng
+
+    except Exception as e:
+        response["error"] = e
+        status_code = 500
+
+
+    return jsonify(response), status_code
 
